09c8bed0bbd107ff90bcfb5499bf15dc2f986ded
Guillaume Outters <guillaume-pdt@outters.eu>
2020-03-15 21:38:40 +0100

    maj.php
    + Majeur (celui qui fait les Mises-À-Jour pour monter en version le schéma de base de données).
    
    darcs-hash:ab8f4120bcfcb963cee572e1de03a6b2efd6e72e

---
+++ app/maj.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once dirname(__FILE__).'/../vendor/gui/majeur/Majeur.php';
+require_once dirname(__FILE__).'/../vendor/gui/majeur/MajeurSiloPdo.php';
+require_once dirname(__FILE__).'/../vendor/gui/majeur/MajeurListeurDossiers.php';
+require_once dirname(__FILE__).'/../vendor/gui/majeur/MajeurJoueurPdo.php';
+
+require_once dirname(__FILE__).'/app.php';
+
+class MajeurRiquet extends Majeur
+{
+	public function __construct($app)
+	{
+		$this->app = $app;
+		
+		$silo = new MajeurSiloPdo($this->app->bdd);
+		
+		$listeur = new MajeurListeurDossiers(dirname(__FILE__).'/maj/maj-(?P<version>[0-9][.0-9]*)(?:-[^/]*)?\.(?:sql|php)');
+		
+		$joueur = new MajeurJoueurPdo($this->app->bdd);
+		
+		parent::__construct($silo, array($listeur), array($joueur));
+	}
+}
+
+$app = new App();
+$m = new MajeurRiquet($app);
+$m->tourner();
+
+?>
