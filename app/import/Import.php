74978539ddc666fa28eb2a46032521a2b7471205
Guillaume Outters <guillaume-pdt@outters.eu>
2021-05-28 04:19:43 +0200

    Import.php
    + Classe d'utilitaires pour importer du SQL Riquet.
    
    darcs-hash:5689024c668f1de4a46382f726b46bbdff54367a

---
+++ app/import/Import.php
@@ -0,0 +1,40 @@
+<?php
+
+class Import
+{
+	protected function _pondreFiche($l)
+	{
+		foreach($l as $c => & $v)
+			$v = $this->formaterSql($c, $v);
+		// À FAIRE: update si déjà présent (pour conserver des liens qui auraient été mis manuellement, hors source, et qui sautent donc si l'on réimporte par effacement puis import total).
+		$req = $this->_insert($l);
+		
+		echo $req;
+	}
+	
+	protected function _insert($l)
+	{
+		return "insert into f (".implode(", ", array_keys($l)).") values (".implode(", ", $l).");\n";
+	}
+	
+	public function formaterSql($c, $v)
+	{
+		switch($c)
+		{
+			case 'ctime':
+			case 'dtime':
+				$v = preg_replace('#^([0-9]{2})/([0-9]{2})/([0-9]{4})#', '\3-\2-\1', $v);
+				break;
+		}
+		
+		if($v == '')
+			$v = 'null';
+		// À FAIRE: else if numérique
+		else
+			$v = "'".strtr($v, array("'" => "''"))."'";
+		
+		return $v;
+	}
+}
+
+?>
