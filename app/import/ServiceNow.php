a6bd0fda5c9a7127257728a971e34f29d3d9c2d5
Guillaume Outters <guillaume-pdt@outters.eu>
2021-05-29 22:40:34 +0200

    ServiceNow: nouveaux champs
    + Remontée de nouveaux champs qui pourraient être d'intérêt.
    - Mais pour le moment en ignorés.
    
    darcs-hash:25f9c058bdbb83b1e003cb880b7f7c7332a2c477

--- app/import/ServiceNow.php
+++ app/import/ServiceNow.php
@@ -16,12 +16,23 @@ class ServiceNow
 		'sys_created_on'             => null,
 		'opened_at'                  => 'ctime', // Utilisé de préférence à sys_created_on (car reflétant son heure de déclaration alors que sys_created_on donne l'heure d'arrivée dans ServiceNow, qui peut différer si la décl a eu lieu dans un autre système puis a été importée dans ServiceNow).
 		'closed_at'                  => 'dtime',
+		'resolved_at'                => null, // Parfois une tâche fermée n'a pas de date de fermeture, mais uniquement une de résolution.
+		'business_duration'          => null,
 		'short_description'          => 'nom',
+		//'description'                => null,
+		//'comments_and_work_notes'    => null,
 		'parent'                     => '@^', // Parent
 		'assignment_group'           => '@>', // Affectation
+		'group_list'                 => null,
+		'assigned_to'                => null,
+		'additional_assignee_list'   => null,
+		'u_contact_type'             => '@o', // Origine
+		'category'                   => null,
+		'subcategory'                => null,
 		'urgency'                    => '@p', // Poids / priorité
 		'priority'                   => '@p', // Poids / priorité
 		'state'                      => '@e', // État
+		//'close_code'                 => null,
 	);
 }
 
