4b13dcdec3cf94424e53a7bb490bf6b38a28f210
Guillaume Outters <guillaume-pdt@outters.eu>
2021-05-29 22:14:04 +0200

    Import: plusieurs _pondreXxx()
    + _pondreSupprLiens(): supprime les liens existants depuis des fiches.
    + _pondreFicheSubst(): crée une fiche avec quelques infos minimales.
    + _pondreLien(): génère le code SQL pour créer un lien vers une fiche.
    
    darcs-hash:1eceadbabeee68dcd337f32fbbc56d9afdd2a108

--- app/import/Import.php
+++ app/import/Import.php
@@ -4,6 +4,14 @@ require_once dirname(__FILE__).'/../Sql.php';
 
 class Import
 {
+	static $TYPE_FICHE_SUBST = array
+	(
+		'>' => 'p',
+		'e' => 'tÉtat',
+		'p' => 'tPoids',
+		'o' => 's',
+	);
+	
 	public function __construct()
 	{
 		$this->sql = new Sql();
@@ -17,6 +25,57 @@ class Import
 		$req = $this->_insert($l);
 		
 		echo $req;
+	}
+	
+	/**
+	 * Émet une destruction de tous les liens d'un ou plusieurs types, partant d'un lot de fiches identifiées.
+	 */
+	protected function _pondreSupprLiens($source, $champId, $ids, $typesLien)
+	{
+		// À FAIRE: $source.
+		echo $this->sql->req("delete from l where a in (select id from f where $champId in (%s)) and t in (%s);\n", $ids, $typesLien);
+	}
+	
+	/**
+	 * Crée une ébauche de fiche.
+	 */
+	protected function _pondreFicheSubst($source, $type, $nom, $champ = 'nom')
+	{
+		// À FAIRE: $source.
+		$values = array_map(function($x) use($type) { return array($x, $type); }, is_array($nom) ? $nom : array($nom));
+		// L'on conflict ne marche qu'avec définition d'une contrainte, or selon le champ les doublons peuvent être autorisés ou non: impossible de mettre une clé d'unicité sur l'ensemble des champs (dont le nom), ce qui entraînerait des blocages sur les types où le nom n'est pas déterminant.
+		// On émule donc à la mimine.
+		// De plus, argh, SQLite ne veut pas de pseudo-table from (values (a, b)) as t(a, b): on doit passer par une table temporaire :-\
+		//echo $this->sql->req("insert into f ($champ, t) values %{s,s} on conflict ($champ, t) do nothing;\n", $values);
+		echo $this->sql->req
+		(
+"drop table if exists t_f;
+create temporary table t_f as select $champ, t from f limit 0;
+insert into t_f ($champ, t) values %{s,s};
+insert into f ($champ, t) select t.* from t_f t left join f on (f.t = t.t and f.$champ = t.$champ) where f.$champ is null;\n",
+			$values
+		);
+	}
+	
+	/**
+	 * Tire un lien de type $typeLien des entrées $orig vers les entrées $valCible. $typeCible permet de restreindre la recherche à un type.
+	 */
+	protected function _pondreLien($source, $champOrig, $orig, $typeLien, $champCible, $valCible, $typeCible = null)
+	{
+		// À FAIRE: $source.
+		$values = array_map(function($x) use($typeLien) { return array($x, $typeLien); }, is_array($orig) ? $orig : array($orig));
+		$filtreCible = isset($typeCible) ? " and b.t in (%s)" : "";
+		
+		$req =
+"insert into l (a, b, t)
+	select a.id, max(b.id), %s
+	from f a, f b
+	where a.$champOrig in (%s) and b.$champCible in (%s)$filtreCible
+	group by a.id;\n";
+		if($filtreCible)
+			echo $this->sql->req($req, $typeLien, $orig, $valCible, $typeCible);
+		else
+			echo $this->sql->req($req, $typeLien, $orig, $valCible);
 	}
 	
 	protected function _insert($l)
