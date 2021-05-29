c3ad44ded6b7b6b1301257ba03d4f7c9e96a44eb
Guillaume Outters <guillaume-pdt@outters.eu>
2021-05-29 22:36:46 +0200

    ServiceNow: listes de champs
    - Des listes plus ou moins officielles de champs traînent sur internet, on le note pour exploitation future.
    
    darcs-hash:661fbdfe9fe1eb7239b56b8df7455a30865dd0cd

--- app/import/ServiceNow.php
+++ app/import/ServiceNow.php
@@ -4,6 +4,13 @@ class ServiceNow
 {
 	public static $CSV = array
 	(
+		/* Champs standard.
+		 * Théoriques:
+		 *   https://developer.servicenow.com/dev.do#!/reference/api/orlando/rest/c_TableAPI
+		 *   https://cloud-elements.github.io/docs/elements/servicenow/ko/api-documentation.html?elementId=145#!/incidents
+		 * Sur une vraie instance:
+		 *   https://stackoverflow.com/questions/56994277/servicenow-rest-api-get-list-of-column-names (mais requiert un compte autorisé API sur une vraie instance).
+		 */
 		'number'                     => 'num',
 		'sys_id'                     => 'id_ext',
 		//'sys_created_on'             => 'ctime',
