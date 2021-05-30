f9f5bb2ffafaa97a1d52a7368c77eb8e2e5e8a57
Guillaume Outters <guillaume-pdt@outters.eu>
2021-05-31 00:22:33 +0200

    App.classes()
    + Soit une classe demandée en paramètre, déniche ses éventuelles sous-classes dans le dossier local/.
    
    darcs-hash:c6a25e6787ab364c4c04edba9728c274c97dcf1a

--- app/app.php
+++ app/app.php
@@ -14,6 +14,30 @@ class App
 		$this->bdd = new PDO($this->config['bdd']);
 		$this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
+	
+	/**
+	 * Meilleure sous-classe candidate.
+	 */
+	public function classe($ancêtre)
+	{
+		if(!isset($this->_classes))
+		{
+			$this->_classes = array();
+			require_once R.'/vendor/gui/util/classes.inc';
+			foreach(glob(R.'/local/*.php') as $f)
+				if(($classes = Classes::ClassesFichier($f, true)))
+					foreach($classes as $classe)
+						$this->_classes[$classe] = $f;
+		}
+		foreach($this->_classes as $classe => $f)
+		{
+			require_once $f;
+			if(is_a($classe, $ancêtre, true))
+				return $classe;
+		}
+		// À FAIRE: en cas de plusieurs candidats, prendre celui le plus profond dans la hiérarchie.
+		return $ancêtre;
+	}
 }
 
 ?>
