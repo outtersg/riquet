4d74c6f0830f47f9e9b21f8bdbdf0b68c3bf1f8b
Guillaume Outters <guillaume-pdt@outters.eu>
2021-07-20 21:40:05 +0200

    bin/actu.servicenow.sh: $temp
    = Si on est en train d'actualiser juste sur les dernières données, on ne souhaite sans doute pas les résultats cachés il y a deux minutes.
    # Et puis on ne veut pas non plus écraser l'extraction complète.
    
    darcs-hash:679562579d7157a145e2212f2c24161b49d6fe53

--- bin/actu.servicenow.sh
+++ bin/actu.servicenow.sh
@@ -9,21 +9,24 @@ _actu()
 	
 	titre "[[[ IMPORT SERVICENOW ]]]"
 	
-	local suf= opttelech=
+	local suf= opttelech= temp=1
 	
 	while [ $# -gt 0 ]
 	do
 		case "$1" in
 			--init)
 				opttelech="$opttelech --init"
+				temp=
 				;;
 		esac
 		shift
 	done
+	[ -z "$temp" ] || suf="$suff.temp"
 	
 	local csv="$VAR/init.servicenow$suf.csv"
 	local sql="$VAR/init.servicenow$suf.sql"
 	
+	[ -z "$temp" ] || rm -f "$csv"
 	find "$csv" -mmin -120 2> /dev/null | grep -q . || \
 	{
 		titre "Téléchargement des ServiceNow"
