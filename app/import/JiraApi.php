ad192cf29e1230d87af1a1ea9f678c2e191051d9
Guillaume Outters <guillaume-pdt@outters.eu>
2022-01-26 23:36:16 +0100

    JiraApi.faire()
    + Récupère un Jira.
    
    darcs-hash:27ba5ee8a30bb43e3156cb666a81b8451ba95382

--- app/import/JiraApi.php
+++ app/import/JiraApi.php
@@ -8,6 +8,40 @@ class JiraApi
 		$this->_idMdp = $idMdp;
 	}
 	
+	/**
+	 * Parcourt un graphe JIRA en partant d'$àFaire.
+	 * 
+	 * @param array $àFaire Numéro des JIRA par lesquels attaquer le graphe.
+	 * @param int|null $plaf Plafond de liens (si un JIRA possède plus de $plaf liens, ceux-ci ne sont pas parcourus).
+	 * @param array $moins Ne pas parcourir les liens de ces JIRA.
+	 * @param array $plus Remonter les liens de ces JIRA même si outre $plaf.
+	 */
+	public function faire($àFaire, $plaf, $moins, $plus)
+	{
+		$faits = array();
+		$liens = array();
+		
+		while($num = array_shift($àFaire))
+		{
+			$this->_aff($num);
+			$j = $this->api('GET', '/issue/'.$num);
+			$j = $j->fields; // L'enrobage ne nous intéresse pas.
+			$cr = $j->summary;
+			$liens = array();
+			$liés = array();
+			if(isset($j->issuelinks))
+				foreach($j->issuelinks as $lien)
+				{
+					if(isset($lien->outwardIssue)) { $de = $num; $liés[$vers = $lien->outwardIssue->key] = 1; }
+					else { $vers = $num; $liés[$de = $lien->inwardIssue->key] = 1; }
+					$liens[$lien->type->inward][$de][$vers] = 1;
+				}
+			if(count($liés))
+				$cr .= ' [-> '.implode(', ', array_keys($liés)).']';
+			$this->_aff($num, $cr);
+		}
+	}
+	
 	protected function _aff($num, $rés = null)
 	{
 		if(!$rés)
