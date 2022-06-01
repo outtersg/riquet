60556a13c011e3329093c6279919682a65fd2cb0
Guillaume Outters <guillaume-pdt@outters.eu>
2022-06-01 23:36:30 +0200

    import/JiraSource: mode mémoire expérimental
    = Mode direct sans passer par la base, pour aller taper le vrai serveur Jira et sortir son résultat sans le persister en base.
    # Très expérimental, pas dit que ça marche ni que ce soit maintenu.
    
    darcs-hash:76f5a6af8867bfb9ebde479972ee4a3ac83cb9f0

--- app/import/JiraSource.php
+++ app/import/JiraSource.php
@@ -6,16 +6,20 @@ require_once R.'/app/import/JiraImport.php';
 
 class JiraSource extends Source
 {
-	public function __construct($app)
+	public function __construct($app, $courtCircuiterLaBase = false)
 	{
 $classe = $app->classe('JiraApi');
 $cji = $app->classe('JiraImport');
 		if(isset($GLOBALS['argv']))
 fprintf(STDERR, "Extraction via [ %s -> %s ]\n", $classe, $cji);
-$ji = new $cji();
+		$ji = new $cji($courtCircuiterLaBase);
+		if(!$courtCircuiterLaBase)
 		$ji->bdd = $app->bdd;
-$j = new $classe($app->config['jira'], $app->config['jira.idmdp'], $ji);
+		// Si on court-circuite la base, les liens dénichés doivent être émis au fur et à mesure et non pas en une fois à la fin,
+		// car le court-circuitage de base indique que l'on est pilotés directement par un Parcours, dont le principe même est de découvrir le graphe en suivant les liens.
+		$j = new $classe($app->config['jira'], $app->config['jira.idmdp'], $ji, $courtCircuiterLaBase);
 		$this->import = $j;
+		$this->persisteur = $ji;
 	}
 }
 
